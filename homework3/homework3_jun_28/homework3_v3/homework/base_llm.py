d06602b72bbbd17d1d498c32a93f50efc028ac6e
ht8956 <hariprasad.thulasiraj@utexas.edu>
2026-07-01 23:40:52 -0500

    commit

--- homework3/homework3_jun_28/homework3_v3/homework/base_llm.py
+++ homework3/homework3_jun_28/homework3_v3/homework/base_llm.py
@@ -105,7 +105,40 @@ class BaseLLM:
                 for r in self.batched_generate(prompts[idx : idx + micro_batch_size], num_return_sequences, temperature)
             ]
 
-        raise NotImplementedError()
+        self.tokenizer.padding_side = "left"
+        if self.tokenizer.pad_token_id is None:
+            self.tokenizer.pad_token = self.tokenizer.eos_token
+
+        inputs = self.tokenizer(prompts, padding=True, return_tensors="pt")
+        inputs = {key: value.to(self.device) for key, value in inputs.items()}
+
+        generation_kwargs = {
+            "input_ids": inputs["input_ids"],
+            "attention_mask": inputs["attention_mask"],
+            "max_new_tokens": 50,
+            "eos_token_id": self.tokenizer.eos_token_id,
+        }
+        if num_return_sequences is not None:
+            generation_kwargs["num_return_sequences"] = num_return_sequences
+        if temperature > 0:
+            generation_kwargs["do_sample"] = True
+            generation_kwargs["temperature"] = temperature
+        else:
+            generation_kwargs["do_sample"] = False
+
+        with torch.no_grad():
+            outputs = self.model.generate(**generation_kwargs)
+
+        prompt_length = inputs["input_ids"].shape[1]
+        generations = self.tokenizer.batch_decode(outputs[:, prompt_length:], skip_special_tokens=True)
+
+        if num_return_sequences is None:
+            return generations
+
+        return [
+            generations[idx : idx + num_return_sequences]
+            for idx in range(0, len(generations), num_return_sequences)
+        ]
 
     def answer(self, *questions) -> list[float]:
         """
