2bc779eaa4ce565cf859ae8c21e579b3098429d4
ht8956 <hariprasad.thulasiraj@utexas.edu>
2026-07-04 14:16:02 -0500

    Update sft.py

--- homework3/homework3_jun_28/homework3_v3/homework/sft.py
+++ homework3/homework3_jun_28/homework3_v3/homework/sft.py
@@ -89,7 +89,7 @@ def train_model(
 
     llm = BaseLLM()
 
-    lora_rank = int(kwargs.pop("r", 8))
+    lora_rank = int(kwargs.pop("r", 16))
     lora_alpha = int(kwargs.pop("lora_alpha", 4 * lora_rank))
 
     lora_cfg = LoraConfig(
@@ -114,7 +114,7 @@ def train_model(
         output_dir=output_dir,
         logging_dir=output_dir,
         report_to="tensorboard",
-        learning_rate=float(kwargs.pop("learning_rate", 1e-4)),
+        learning_rate=float(kwargs.pop("learning_rate", 5e-4)), 
         num_train_epochs=float(kwargs.pop("num_train_epochs", 5)),
         per_device_train_batch_size=int(kwargs.pop("per_device_train_batch_size", 32)),
         gradient_checkpointing=True,
